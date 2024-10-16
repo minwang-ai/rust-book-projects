cf8f502390acaa5044b997210b40937910d8ad91
Min Wang <professional@minwang-ai.com>
2024-10-16 11:44:01 +0000

    toy example

---
+++ hello_cargo/target/debug/deps/hello_cargo-70b7650f2196efb1.d
@@ -0,0 +1,5 @@
+/workspaces/rust-book-projects/hello_cargo/target/debug/deps/hello_cargo-70b7650f2196efb1: src/main.rs
+
+/workspaces/rust-book-projects/hello_cargo/target/debug/deps/hello_cargo-70b7650f2196efb1.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/workspaces/rust-book-projects/hello_cargo/target/debug/hello_cargo: /workspaces/rust-book-projects/hello_cargo/src/main.rs
